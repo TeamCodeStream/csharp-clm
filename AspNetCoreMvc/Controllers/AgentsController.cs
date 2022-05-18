f449d19e1aa9b0fb2d35382dff439c9a949d5f7c
Jacob Affinito <jaffinito@newrelic.com>
2022-05-18 12:36:00 -0700

    initial commit

---
+++ AspNetCoreMvc/Controllers/AgentsController.cs
@@ -0,0 +1,47 @@
+﻿using AspNetCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace AspNetCoreMvc.Controllers
+{
+	public class AgentsController : Controller
+	{
+		private readonly ILogger<AgentsController> _logger;
+
+		public AgentsController(ILogger<AgentsController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Show()
+		{
+			var helpers = new Helpers();
+			ViewData["InstanceMethod"] = helpers.CustomMethodOne("Wake me. When you need me.");
+			ViewData["StaticMethod"] = Helpers.CustomStaticMethodOne("It's dangerous to go alone, take this!");
+			return View();
+		}
+
+		public IActionResult Create()
+		{
+			var helpers = new Helpers();
+			ViewData["InstanceMethod"] = helpers.CustomMethodOne("You are not prepared!");
+			ViewData["StaticMethod"] = Helpers.CustomStaticMethodOne("You Must Construct Additional Pylons!");
+			return View();
+		}
+
+		public IActionResult Destroy()
+		{
+			var helpers = new Helpers();
+			ViewData["InstanceMethod"] = helpers.CustomMethodOne("It's time to kick ass and chew bubblegum, and I'm all out of gum.");
+			ViewData["StaticMethod"] = Helpers.CustomStaticMethodOne("Survival can be a matter of luck or skill. And you can't rely on luck.");
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ AspNetCoreMvc/Helpers.cs
@@ -0,0 +1,37 @@
+﻿using NewRelic.Api.Agent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMvc
+{
+	public class Helpers
+	{
+		[Trace]
+		public static string CustomStaticMethodOne(string data)
+		{
+			var dataFromStaticMethodTwo = CustomStaticMethodTwo();
+			return data + " @ " + dataFromStaticMethodTwo;
+		}
+
+		[Trace]
+		private static string CustomStaticMethodTwo()
+		{
+			return DateTime.Now.ToLongTimeString();
+		}
+
+		[Trace]
+		public string CustomMethodOne(string data)
+		{
+			var dataFromMethodTwo = CustomMethodTwo();
+			return data + " @ " + dataFromMethodTwo;
+		}
+
+		[Trace]
+		private string CustomMethodTwo()
+		{
+			return DateTime.Now.ToShortTimeString();
+		}
+	}
+}
